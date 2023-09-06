8a758ef8d0ae52de33b42ce43c8c3ac2be5df269
ErnstOsbeck <143172129+ErnstOsbeck@users.noreply.github.com>
2023-09-06 10:41:06 +0200

    Add project files.

---
+++ Uppgift 1.2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace uppgift_1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;//ändrar bagrunden till texten till blå
+            Console.Clear();//gör så att bakgrundsfärgen sprids ut över hela fönstret, inte bara brecis bakom texten
+            Console.ForegroundColor = ConsoleColor.White;//ändrar textfärgen till vit
+            Console.WriteLine("Din dator har kraschat. Det går inte att fixa.");//crashmeddelande
+            Console.WriteLine("Vänligen kasta i en industriell masugn.");
+        }
+    }
+}
